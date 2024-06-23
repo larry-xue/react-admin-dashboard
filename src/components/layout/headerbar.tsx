2b137561765d20fb3c9c085124cb0c0e9163bd0b
larry-xue <azoux0311@gmail.com>
2024-06-23 16:46:19 +0800

    feat: add link

--- src/components/layout/headerbar.tsx
+++ src/components/layout/headerbar.tsx
@@ -1,5 +1,6 @@
 
 import { Layout } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 const { Header } = Layout;
 
 const Headerbar = (props: { colorBgContainer: string }) => {
@@ -7,9 +8,10 @@ const Headerbar = (props: { colorBgContainer: string }) => {
     <Header title='React Admin Dashboard' style={{ padding: 0, background: props.colorBgContainer }}>
       <div style={{ display: 'flex', alignItems: 'center', height: '100%', padding: "0 20px", justifyContent: 'space-between' }}>
         <h2>React Admin Dashboard</h2>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
           <p style={{ marginRight: 10 }}>Larry Xue</p>
           <img src="https://avatars.githubusercontent.com/u/48818060?s=48&v=4" alt="avatar" style={{ width: 40, height: 40 }} />
+          <GithubOutlined style={{ fontSize: 30 }} onClick={() => window.open('https://github.com/larry-xue/react-admin-dashboard')} />
         </div>
       </div>
     </Header>
